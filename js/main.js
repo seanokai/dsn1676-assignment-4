e06b5c102e01e762b86ac450d05f02a389d64090
Sean Okai <seanokai@hotmail.com>
2015-04-23 02:25:13 -0400

    rewrote javascript with necessary tags

--- js/main.js
+++ js/main.js
@@ -22,3 +22,20 @@ carbohydrates: 13.8,
 protein: 1.4
 };
 
+var writeBerries = function (berries) {
+  document.write('<h1>Berries and Facts</h1>');
+
+  berries.forEach(function (item) {
+    var shrub = (item.onShrub) ? 'Yes' : 'No';
+
+    document.write('<h2>' + item.name + '</h2>');
+    document.write('<dl>');
+    document.write('<dt>Grows on a shrub?</dt><dd>' + shrub + '</dd>');
+    document.write('<dt>Energy</dt><dd>' + item.energy + ' kJ</dd>');
+    document.write('<dt>Carbohydrates</dt><dd>' + item.carbs + ' g</dd>');
+    document.write('<dt>Protein</dt><dd>' + item.protein + ' g</dd>');
+    document.write('</dl>');
+  });
+}
+
+writeBerries([blueberries, grapes, redCurrant]);
