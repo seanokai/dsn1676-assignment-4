21d065660fb03fcd93dfc17a19bbe7ed968a9c1c
Sean Okai <seanokai@hotmail.com>
2015-04-23 02:20:26 -0400

    added main.js input content

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var blueberries = {
+info: 'Vaccinium corymbosum',
+growsOnShrub: 'true',
+energy: 240,
+carbohydrates: 14.49,
+protein: 0.74
+};
+  
+var grapes = {
+info: 'Vitis vinifera',
+growsOnShrub: 'false',
+energy: 288,
+carbohydrates: 18.1,
+protein: 0.72,
+};
+
+var redCurrants = {
+info: 'Ribes rubrum',
+growsOnShrub: 'true',
+energy: 234,
+carbohydrates: 13.8,
+protein: 1.4
+};
+
