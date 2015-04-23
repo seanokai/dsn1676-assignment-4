48b29c41b3dfd2775efe4f8e5051b114afa345d3
Sean Okai <seanokai@hotmail.com>
2015-04-23 02:43:23 -0400

    added content to javascript

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 var blueberries = {
-info: 'Vaccinium corymbosum',
-growsOnShrub: 'true',
-energy: 240,
-carbohydrates: 14.49,
-protein: 0.74
-};
-  
-var grapes = {
-info: 'Vitis vinifera',
-growsOnShrub: 'false',
-energy: 288,
-carbohydrates: 18.1,
-protein: 0.72,
+  name: 'Blueberries',
+  latin: 'Vaccinium corymbosum',
+  onShrub: true,
+  energy: 240,
+  carbs: 14.49,
+  protein: 0.74
 };
 
-var redCurrants = {
-info: 'Ribes rubrum',
-growsOnShrub: 'true',
-energy: 234,
-carbohydrates: 13.8,
-protein: 1.4
+var grapes = {
+  name: 'Grapes',
+  latin: 'Vitis vinifera',
+  onShrub: false,
+  energy: 288,
+  carbs: 18.1,
+  protein: 0.72
+};
+
+var redCurrant = {
+  name: 'Red currant',
+  latin: 'Ribes rubrum',
+  onShrub: true,
+  energy: 234,
+  carbs: 13.8,
+  protein: 1.4
 };
 
 var writeBerries = function (berries) {
@@ -29,13 +32,20 @@ var writeBerries = function (berries) {
     var shrub = (item.onShrub) ? 'Yes' : 'No';
 
     document.write('<h2>' + item.name + '</h2>');
+    
     document.write('<dl>');
+    
     document.write('<dt>Grows on a shrub?</dt><dd>' + shrub + '</dd>');
+    
     document.write('<dt>Energy</dt><dd>' + item.energy + ' kJ</dd>');
+    
     document.write('<dt>Carbohydrates</dt><dd>' + item.carbs + ' g</dd>');
+    
     document.write('<dt>Protein</dt><dd>' + item.protein + ' g</dd>');
+    
     document.write('</dl>');
   });
 }
 
 writeBerries([blueberries, grapes, redCurrant]);
+
